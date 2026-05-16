472ae2287816438696341b5ea04c735134a0924e
PeteJuli <Julian-Petersmann@hotmail.com>
2026-05-16 23:24:00 +0200

    Updated User Agent

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -20,7 +20,7 @@ int main()
     
     // User-Agent (Reduce Ban risk by using a common one)
     const char* user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-                             "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+                             "(KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36";
 
     // Set the user agent
     webkit_settings_set_user_agent(settings, user_agent);
